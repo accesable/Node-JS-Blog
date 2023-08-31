e183fb0c39339f015b466c7710194f8369d4fe21
accesable <trannhutanh654@gmail.com>
2023-08-31 15:06:11 +0700

    Mongo DB Connection

--- src/index.js
+++ src/index.js
@@ -9,6 +9,22 @@ const route = require('./routes')
 
 app.use(morgan('combined'))
 
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  try {
+       mongoose.connect('mongodb://127.0.0.1:27017/Blog_JS');
+      console.log("Connect Successfully! Yahhh!")
+  } catch (error) {
+    console.log("Cannot connect to MongoDB") 
+  }
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
 app.use(express.static(path.join(__dirname,'public')))
 app.use(express.urlencoded({
   extended:true
